09d4ac8c10d0deafd764ac1c37a7a0e85cc2cbd1
arno-o <arno.oosterlynck@student.howest.be>
2026-01-16 15:42:21 +0100

    Created bot class

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import StartGame from './game/main';
 import { insertCoin } from 'playroomkit';
+import { PlayerBot } from '@/player/Bot';
 
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         await insertCoin({
             enableBots: true,
             maxPlayersPerRoom: 6,
-            // botOptions: {
-            //     botClass: OhHellBot,
-            //     botParams: {
-            //         // Initial params if needed
-            //         score: 0
-            //     }
-            // }
+            botOptions: {
+                botClass: PlayerBot,
+                botParams: {
+                    // Initial params if needed
+                    score: 0
+                }
+            }
         });
         
         StartGame('game-container');
---
+++ src/player/Bot.ts
@@ -0,0 +1,7 @@
+import { Bot } from "playroomkit";
+
+export class PlayerBot extends Bot {
+    constructor(botParams: Object) {
+        super(botParams);
+    }
+}
